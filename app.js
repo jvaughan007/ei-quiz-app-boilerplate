db73eb7b29fc4d114a543bab7d6d2d9d130ad051
Joshua Vaughan <jvaughan007@yahoo.com>
2020-09-16 21:35:10 -0500

    pseudocoded out all functionality and wireframed the templates.

--- app.js
+++ app.js
@@ -51,71 +51,83 @@ const store = {
 // The below functions return HTML templates
 
 function generateWelcomePageTemplate() {
-  // code needs to generate the welcome page template to be displayed on the DOM in the main
+  // Code needs to generate the welcome page template to be displayed on the DOM in the main
 
-  // must be usable by a keyboard and mouse******
+  // Must be usable by a keyboard and mouse******
 
-  // must contain a start quiz button that - on click - changes from this template to the question/answer template 
+  // Must contain a start quiz button that - on click - changes from this template to the question/answer template and changes quiz start to true
   
-  // per the generateResultsTemplate function, the quiz score needs to revert to 0 when you reach this page - possibly when you hit the start quiz button?
+  // Per the generateResultsTemplate function, the score , quizStart, and questionNumber need to be reset when you reach this page - possibly when you hit the retry button?
 
-  // must render at the end
+  // Must render at the end
 }
 
 function generateQuestionTemplate() {
-  // code needs to create the page template for the question div of the quiz
+  // Code needs to create the page template for the question div of the quiz
 
-  // must be usable by a keyboard and mouse******
+  // Must be usable by a keyboard and mouse******
 
   // Needs to pull the question from the store
 
-  // must call the render function at end
+  // Must call the render function at end
 }
 
 function generateAnswerTemplate() {
-  // code needs to create a template for the answer choices div of the quiz ***must be in a form***
+  // Code needs to create a template for the answer choices div of the quiz ***must be in a form***
 
-  // must be usable by a keyboard and mouse******
+  // Must be usable by a keyboard and mouse******
 
   // Needs to pull the answers from the store and orient them in a radial selector form
 
   // Upon submitting an answer, users should:
-    // * receive textual feedback about their answer. If they were incorrect, they should be told the correct answer.
-    // * be moved onto the next question (or interact with an element to move on).
+  // * receive textual feedback about their answer. If they were incorrect, they should be told the correct answer.
+  // * be moved onto the next question (or interact with an element to move on).
 
-  // must call the render function at end if a standalone div
+  // Must call the render function at end if a standalone div
 }
 
-function generateCurrentScoreTemplate() {
-  // code needs to create a template for the div that will display the total score of the quiz so far
+function generateFeedbackTemplate() {
+  // Code needs to create a teplate for displaying feedback
 
-  // must be usable by a keyboard and mouse******
+  // Create an if statement that checks if the submitted answer choice was the correct answer
+  //*** / 1.) If right, it will render a template with a happy meseeks with a positive statement and the running score
+  //*** // 2.) If wrong, it will render a template with a meseeks filled with existential terror, a wrong answer informative statement and the running score
+  
+  // Must have a 'next question' button at the end, ***regardless of if statement result***
+
+  // Must call the render function at end
+} 
+
+function generateCurrentScoreTemplate() {
+  // Code needs to create a template for the div that will display the total score of the quiz so far
+
+  // Must be usable by a keyboard and mouse******
 
   // Needs to pull the question number at that point in the quiz from the store
 
   // Needs to reset the score after the quiz has been reset to the welcome page
 
-  // must call the render function at end if a standalone div
+  // Must call the render function at end if a standalone div
 }
 
 function generateQuestionNumberTemplate() {
-  // code needs to create a template for the div that will show which question they're on (i.e. - Question 7 of 10)
+  // Code needs to create a template for the div that will show which question they're on (i.e. - Question 7 of 10)
 
-  // must be usable by a keyboard and mouse******
+  // Must be usable by a keyboard and mouse******
 
   // Needs to pull the quiz score at that point in the quiz from the store
 
-  // must call the render function at end if a standalone div
+  // Must call the render function at end if a standalone div
 }
 
 function generateResultsTemplate() {
-  // code needs to create a template for the div that will show the final results page
+  // Code needs to create a template for the div that will show the final results page
 
   // Users should be shown their overall score at the end of the quiz. In other words, how many questions they got right out of the total questions asked.
 
   // Users should be able to start a new quiz. ****add a button*** (button could theoretically reset the value of quiz score to 0?)
 
-  // must call the render function at end
+  // Must call the render function at end
 }
 
 
